d74d63014abb1161670ae97cca64af7643570560
Falcosc <Falcosc@users.noreply.github.com>
2021-08-04 21:55:12 +0200

    File Integrity: resolve progress info race condition and handle unexpected parsing issues

--- source/file-integrity/include/ProgressInfo.php
+++ source/file-integrity/include/ProgressInfo.php
@@ -37,7 +37,7 @@ if ($_POST['disk']>0) {
     echo file_get_contents($tmp);
   } else {
     echo file_exists($end) ? file_get_contents($end) : "100%#<span class='red-text red-button'>"._('Error')."</span>"._('Operation aborted')."#";
-    @unlink($end);
+    //don't delete end file because there could be a race condition if you submit forms or reload the page for any other reason
   }
 } else {
   $ctrl = "/var/tmp/ctrl.tmp";
