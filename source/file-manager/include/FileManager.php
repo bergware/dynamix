263b261273b21bb526f75d0d790802bef29bf48f
bergware <bergware@users.noreply.github.com>
2021-12-29 01:24:45 +0100

    Dynamix plugin update

--- source/file-manager/include/FileManager.php
+++ source/file-manager/include/FileManager.php
@@ -16,18 +16,20 @@ $source  = explode("\n",htmlspecialchars_decode(rawurldecode($_POST['source'])))
 $target  = rawurldecode($_POST['target']);
 $H       = empty($_POST['hdlink']) ? '' : 'H';
 $protect = empty($_POST['protect']) ? '--ignore-existing' : '';
-$arg1    = preg_replace('/(["\'&()[\]\/])/','\\\\$1',$source[0]);
-$running = '/tmp/file.manager.running';
-$moving  = '/tmp/file.manager.moving';
-$busy    = file_exists($running);
+$status  = '/var/tmp/file.manager.status';
+$moving  = '/var/tmp/file.manager.moving';
+$procid  = '/var/tmp/file.manager.pid';
 $idle    = !file_exists($moving);
 
-function pgrep($proc) {
-  global $arg1;
-  return exec("pgrep -a $proc|awk '/$arg1/{print \$1;exit}'");
+function pgrep($pid) {
+  global $procid;
+  $pid = is_array($pid) ? $pid[0] : $pid;
+  $pid = $pid && file_exists("/proc/$pid") ? $pid : '';
+  $pid ? file_put_contents($procid,$pid) : @unlink($procid);
+  return $pid;
 }
 function truepath($name) {
-  $bits = array_filter(explode('/',$name), 'mb_strlen');
+  $bits = array_filter(explode('/',$name),'mb_strlen');
   $path = [];
   foreach ($bits as $bit) {
     if ($bit == '.') continue;
@@ -44,100 +46,87 @@ function escape($name) {return escapeshellarg(validname($name));}
 function quoted($name) {return is_array($name) ? implode(' ',array_map('escape',$name)) : escape($name);}
 
 $reply['status'] = 'starting';
+$reply['pid'] = file_exists($procid) ? file_get_contents($procid) : null;
 switch ($action) {
 case 0: // create folder
-  if ($busy) {
+  if ($reply['pid']) {
     $reply['status'] = 'creating';
   } else {
-    touch($running);
-    exec("mkdir -p ".quoted($source[0].'/'.$target)." >/dev/null 2>&1 &");
+    exec("mkdir -p ".quoted($source[0].'/'.$target)." >/dev/null 2>&1 & echo $!",$reply['pid']);
   }
-  $reply['pid'] = pgrep('mkdir');
   break;
 case 1: // delete folder
 case 5: // delete file
-  if ($busy) {
+  if ($reply['pid']) {
     $reply['status'] = 'removing';
   } else {
-    touch($running);
-    exec("rm -Rf ".quoted($source)." >/dev/null 2>&1 &");
+    exec("rm -Rf ".quoted($source)." >/dev/null 2>&1 & echo $!",$reply['pid']);
   }
-  $reply['pid'] = pgrep('rm');
   break;
 case 2: // rename folder
 case 6: // rename file
-  if ($busy) {
+  if ($reply['pid']) {
     $reply['status'] = 'renaming';
   } else {
-    touch($running);
     $path = dirname($source[0]);
-    exec("mv -f ".quoted($source)." ".quoted("$path/$target")." >/dev/null 2>&1 &");
+    exec("mv -f ".quoted($source)." ".quoted("$path/$target")." >/dev/null 2>&1 & echo $!",$reply['pid']);
   }
-  $reply['pid'] = pgrep('mv');
   break;
 case 3:  // copy folder
 case 7:  // copy file
-  if ($busy) {
-    $reply['status'] = preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $running|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'copying';
+  if ($reply['pid']) {
+    $reply['status'] = preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $status|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'copying';
   } else {
     $target = validname($target,false);
     if ($target) {
-      touch($running);
-      exec("rsync -ahPIX$H $protect --mkpath --info=name0,progress2 ".quoted($source)." ".escapeshellarg($target)." >$running 2>/dev/null &");
+      exec("rsync -ahPIX$H $protect --mkpath --info=name0,progress2 ".quoted($source)." ".escapeshellarg($target)." >$status 2>/dev/null & echo $!",$reply['pid']);
     } else {
       $reply['error'] = 'Invalid target';
     }
   }
-  $reply['pid'] = pgrep('rsync');
   break;
 case 4: // move folder
 case 8: // move file
-  if ($busy) {
-    $reply['status'] = $idle ? 'moving' : (preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $running|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'moving');
+  if ($reply['pid']) {
+    $reply['status'] = $idle ? 'moving' : (preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $status|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'moving');
   } else {
     $target = validname($target,false);
     if ($target) {
-      touch($running);
       touch($moving);
       $idle = false;
-      exec("rsync -ahPIX$H $protect --mkpath --info=name0,progress2 --remove-source-files ".quoted($source)." ".escapeshellarg($target)." >$running 2>/dev/null &");
+      exec("rsync -ahPIX$H $protect --mkpath --info=name0,progress2 --remove-source-files ".quoted($source)." ".escapeshellarg($target)." >$status 2>/dev/null & echo $!",$reply['pid']);
     } else {
       $reply['error'] = 'Invalid target';
     }
   }
-  $reply['pid'] = $idle ? pgrep('find') : pgrep('rsync');
   break;
 case 9: // change owner
-  if ($busy) {
+  if ($reply['pid']) {
     $reply['status'] = 'updating';
   } else {
-    touch($running);
-    exec("chown -Rf $target ".quoted($source)." >/dev/null 2>&1 &");
+    exec("chown -Rf $target ".quoted($source)." >/dev/null 2>&1 & echo $!",$reply['pid']);
   }
-  $reply['pid'] = pgrep('chown');
   break;
 case 10: // change permission
-  if ($busy) {
+  if ($reply['pid']) {
     $reply['status'] = 'updating';
   } else {
-    touch($running);
-    exec("chmod -Rf $target ".quoted($source)." >/dev/null 2>&1 &");
+    exec("chmod -Rf $target ".quoted($source)." >/dev/null 2>&1 & echo $!",$reply['pid']);
   }
-  $reply['pid'] = pgrep('chmod');
   break;
 case 99: // kill background processes
-  foreach (['rm','mv','rsync','chmod','chown'] as $proc) exec("pkill -x $proc");
-  $reply['pid'] = '';
+  exec("kill -9 ".$reply['pid']);
   break;
 }
+$reply['pid'] = pgrep($reply['pid']);
 if (empty($reply['pid'])) {
   if ($idle) {
-    if (file_exists($running)) unlink($running);
+    if (file_exists($status)) unlink($status);
     $reply['status'] = 'done';
   } else {
     unlink($moving);
-    exec("find ".quoted($source)." -type d -empty -delete >/dev/null 2>&1 &");
-    $reply['pid'] = 1;
+    exec("find ".quoted($source)." -type d -empty -delete >/dev/null 2>&1 & echo $!",$reply['pid']);
+    $reply['pid'] = pgrep($reply['pid']);
   }
 }
 header('Content-Type: application/json');
