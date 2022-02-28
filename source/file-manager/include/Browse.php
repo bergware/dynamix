437c77d8a91bac7f0f9f21d6bb3c3b56be867044
bergware <bergware@users.noreply.github.com>
2022-02-28 12:00:16 +0100

    Dynamix plugin update

--- source/file-manager/include/Browse.php
+++ source/file-manager/include/Browse.php
@@ -1,6 +1,6 @@
 <?PHP
-/* Copyright 2005-2021, Lime Technology
- * Copyright 2012-2021, Bergware International.
+/* Copyright 2005-2022, Lime Technology
+ * Copyright 2012-2022, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
--- source/file-manager/include/FileManager.php
+++ source/file-manager/include/FileManager.php
@@ -1,6 +1,6 @@
 <?PHP
-/* Copyright 2005-2021, Lime Technology
- * Copyright 2012-2021, Bergware International.
+/* Copyright 2005-2022, Lime Technology
+ * Copyright 2012-2022, Bergware International.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
@@ -11,137 +11,11 @@
  */
 ?>
 <?
-$action = $_POST['action'];
-$source = explode("\n",htmlspecialchars_decode(rawurldecode($_POST['source']??'')));
-$target = rawurldecode($_POST['target']??'');
-$H      = empty($_POST['hdlink']) ? '' : 'H';
-$exist  = empty($_POST['exist']) ? '--ignore-existing' : '';
-$status = '/var/tmp/file.manager.status';
-$moving = '/var/tmp/file.manager.moving';
-$error  = '/var/tmp/file.manager.error';
-$procid = '/var/run/file.manager.pid';
-$null   = '/dev/null';
-$idle   = !file_exists($moving);
-
-function pgrep($pid) {
-  global $procid;
-  $pid = is_array($pid) ? $pid[0] : $pid;
-  $pid = $pid && file_exists("/proc/$pid") ? $pid : '';
-  $pid ? file_put_contents($procid,$pid) : @unlink($procid);
-  return $pid;
-}
-function isdir($name) {
-  return mb_substr($name,-1)=='/';
-}
-function truepath($name) {
-  $bits = array_filter(explode('/',$name),'mb_strlen');
-  $path = [];
-  foreach ($bits as $bit) {
-    if ($bit == '.') continue;
-    if ($bit == '..') array_pop($path); else $path[] = $bit;
-  }
-  return '/'.implode('/',$path);
-}
-function validname($name, $real=true) {
-  $path = $real ? realpath(dirname($name)) : truepath(dirname($name));
-  $root = explode('/',$path)[1] ?? '';
-  return in_array($root,['mnt','boot']) ? $path.'/'.basename($name).(mb_substr($name,-1)=='/'?'/':'') : '';
-}
-function escape($name) {return escapeshellarg(validname($name));}
-function quoted($name) {return is_array($name) ? implode(' ',array_map('escape',$name)) : escape($name);}
-
-$reply['status'] = 'starting';
-$reply['pid'] = @file_get_contents($procid);
-switch ($action) {
-case 0: // create folder
-  if ($reply['pid']) {
-    $reply['status'] = 'creating';
-  } else {
-    exec("mkdir -p ".quoted($source[0].'/'.$target)." 1>$null 2>$error & echo $!",$reply['pid']);
-  }
-  break;
-case 1: // delete folder
-case 5: // delete file
-  if ($reply['pid']) {
-    $reply['status'] = 'removing';
-  } else {
-    exec("rm -Rf ".quoted($source)." 1>$null 2>$error & echo $!",$reply['pid']);
-  }
-  break;
-case 2: // rename folder
-case 6: // rename file
-  if ($reply['pid']) {
-    $reply['status'] = 'renaming';
-  } else {
-    $path = dirname($source[0]);
-    exec("mv -f ".quoted($source)." ".quoted("$path/$target")." 1>$null 2>$error & echo $!",$reply['pid']);
-  }
-  break;
-case 3:  // copy folder
-case 7:  // copy file
-  if ($reply['pid']) {
-    $reply['status'] = preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $status|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'copying';
-  } else {
-    $target = validname($target,false);
-    if ($target) {
-      $mkpath = isdir($target) ? '--mkpath' : '';
-      exec("rsync -ahPIX$H $exist $mkpath --info=name0,progress2 ".quoted($source)." ".escapeshellarg($target)." 1>$status 2>$error & echo $!",$reply['pid']);
-    } else {
-      $reply['error'] = 'Invalid target name';
-    }
-  }
-  break;
-case 4: // move folder
-case 8: // move file
-  if ($reply['pid']) {
-    $reply['status'] = $idle ? 'moving' : (preg_replace('/\s\s+/',' ',rtrim(exec("tail -1 $status|grep -Pom1 '^.+ \K[0-9]+%[^(]+'"))) ?: 'moving');
-  } else {
-    $target = validname($target,false);
-    if ($target) {
-      touch($moving);
-      $idle = false;
-      $mkpath = isdir($target) ? '--mkpath' : '';
-      exec("rsync -ahPIX$H $exist $mkpath --info=name0,progress2 --remove-source-files ".quoted($source)." ".escapeshellarg($target)." 1>$status 2>$error & echo $!",$reply['pid']);
-    } else {
-      $reply['error'] = 'Invalid target name';
-    }
-  }
-  break;
-case 9: // change owner
-  if ($reply['pid']) {
-    $reply['status'] = 'updating';
-  } else {
-    exec("chown -Rf $target ".quoted($source)." 1>$null 2>$error & echo $!",$reply['pid']);
-  }
-  break;
-case 10: // change permission
-  if ($reply['pid']) {
-    $reply['status'] = 'updating';
-  } else {
-    exec("chmod -Rf $target ".quoted($source)." 1>$null 2>$error & echo $!",$reply['pid']);
-  }
-  break;
-case 99: // kill running background process
-  if ($reply['pid']) exec("kill -9 ".$reply['pid']);
-  @unlink($moving);
-  $idle = true;
-  break;
-}
-$reply['pid'] = pgrep($reply['pid']);
-if (empty($reply['pid'])) {
-  if ($idle) {
-    @unlink($status);
-    if (file_exists($error)) {
-      $reply['error'] = str_replace("\n","<br>",trim(file_get_contents($error)));
-      unlink($error);
-    }
-    $reply['status'] = 'done';
-  } else {
-    unlink($moving);
-    exec("find ".quoted($source)." -type d -empty -delete 1>$null 2>$null & echo $!",$reply['pid']);
-    $reply['pid'] = pgrep($reply['pid']);
-  }
-}
-header('Content-Type: application/json');
-die(json_encode($reply));
+$data[] = 'action="'.($_POST['action']??'').'"';
+$data[] = 'title="'.rawurldecode($_POST['title']??'').'"';
+$data[] = 'source="'.htmlspecialchars_decode(rawurldecode($_POST['source']??'')).'"';
+$data[] = 'target="'.rawurldecode($_POST['target']??'').'"';
+$data[] = 'H="'.(empty($_POST['hdlink']) ? '' : 'H').'"';
+$data[] = 'exist="'.(empty($_POST['exist']) ? '--ignore-existing' : '').'"';
+file_put_contents('/var/tmp/file.manager.active',implode("\n",$data));
 ?>
